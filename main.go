8cb5bea9155c63a19c33fab1dd6eb63e8e20f60c
gemm123 <gemaakbar213@gmail.com>
2022-06-09 10:13:42 +0700

    done

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "jwt-authentication-golang/database"
+import (
+	"jwt-authentication-golang/controllers"
+	"jwt-authentication-golang/database"
+	"jwt-authentication-golang/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	database.Connect("host=localhost user=postgres password=gemmq123456 dbname=jwt_demo port=5432 sslmode=disable TimeZone=Asia/Shanghai")
 	database.Migrate()
+
+	router := initRouter()
+	router.Run()
+}
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.POST("/token", controllers.GenerateToken)
+		api.POST("/user/register", controllers.RegisterUser)
+		secured := api.Group("/secured").Use(middlewares.Auth())
+		{
+			secured.GET("/ping", controllers.Ping)
+		}
+	}
+
+	return router
 }
